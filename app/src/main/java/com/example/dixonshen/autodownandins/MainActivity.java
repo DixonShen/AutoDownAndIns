389b8c5bc9305269cfddd3be9fb98c66c72f87bb
dixonshen <dixonshen@hotmail.com>
2017-03-24 10:33:04 +0800

    initial commit

--- app/src/main/java/com/example/dixonshen/autodownandins/MainActivity.java
+++ app/src/main/java/com/example/dixonshen/autodownandins/MainActivity.java
@@ -4,12 +4,15 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import java.io.File;
 
@@ -17,6 +20,7 @@ public class MainActivity extends AppCompatActivity {
 
     private Button btn1;
     private EditText et1;
+    private Context mContext;
 
     private BroadcastReceiver receiver;
 
@@ -24,6 +28,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mContext = this;
 
         btn1 = (Button) findViewById(R.id.download1);
         et1 = (EditText) findViewById(R.id.url1);
@@ -31,10 +36,36 @@ public class MainActivity extends AppCompatActivity {
         btn1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startDowbload();
+                if (isNetworkAvailable(mContext)) {
+                    startDowbload();
+                } else {
+                    Toast.makeText(mContext, "网络连接不可用", Toast.LENGTH_SHORT).show();
+                }
             }
         });
 
+    }
+
+    /**
+     * 检测当前的网络（WLAN, 4G/2G）状态
+     * @param context
+     * @return
+     */
+    public static boolean isNetworkAvailable(Context context) {
+        ConnectivityManager connectivityManager = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        if (connectivityManager != null) {
+            NetworkInfo info = connectivityManager.getActiveNetworkInfo();
+            if (info != null && info.isConnected()) {
+                // 当前网络是连接的
+                if (info.getState() == NetworkInfo.State.CONNECTED) {
+                    // 当前所连接的网络可用
+                    return true;
+                } else {
+
+                }
+            }
+        }
+        return false;
     }
 
     public void startDowbload(){
@@ -64,6 +95,15 @@ public class MainActivity extends AppCompatActivity {
             unregisterReceiver(receiver);
         }
         super.onDestroy();
+    }
+
+    private void setResultToToast(final String string){
+        MainActivity.this.runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                Toast.makeText(MainActivity.this, string, Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
 }
